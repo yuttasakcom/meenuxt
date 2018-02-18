420d1d03be74a3123dc72f8112177d7a321fed33
YoProgrammer <yuttasakcom@gmail.com>
2018-02-18 13:03:00 +0700

    create Navbar componetns

---
+++ components/Navbar/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <nav class="teal">
+            <div class="nav-wrapper">
+                <nuxt-link to="/" class="brand-logo">DevMeetup</nuxt-link>
+                
+                <a href="#" data-activates="slide-out" class="button-collapse">
+                   <i class="material-icons">menu</i>
+                </a>
+
+                <ul class="right hide-on-med-and-down">
+                    <nuxt-link tag="li" to="/">
+                        <a>
+                            <i class="material-icons left">supervisor_account</i> View Meetups
+                        </a>
+                    </nuxt-link>
+                </ul>
+            </div>
+        </nav>
+
+        <SideNav />
+    </div>
+</template>
+
+<script>
+import SideNav from './SideNav'
+export default {
+  components: {
+    SideNav
+  }
+}
+</script>
+
--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,27 @@ module.exports = {
       { hid: 'description', name: 'description', content: 'Nuxt.js project' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Open+Sans'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+      },
+      {
+        rel: 'stylesheet',
+        href:
+          'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css'
+      }
+    ],
+    script: [
+      { src: 'https://code.jquery.com/jquery-3.2.1.min.js' },
+      {
+        src:
+          'https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js'
+      }
     ]
   },
   /*
@@ -24,7 +44,7 @@ module.exports = {
     /*
     ** Run ESLint on save
     */
-    extend (config, { isDev, isClient }) {
+    extend(config, { isDev, isClient }) {
       if (isDev && isClient) {
         config.module.rules.push({
           enforce: 'pre',
